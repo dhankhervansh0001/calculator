ab1124912919574e6f2e6dd705bd6b5ec7be35b4
dhankhervansh0001 <dhankhervansh2009@gmail.com>
2026-01-12 10:30:07 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,14 @@
-a = 
+a = int(input("enter a number:"))
+b = int(input("enter a number:"))
+op = input("enter a operetor")
+if op == "+":
+  print(a+b)
+if op =="-":
+  print(a-b)
+if op=="/":
+  print(a/b)
+if op=="*":
+  print(a*b)
+else:
+  print("you enter a another operetor")
+  
